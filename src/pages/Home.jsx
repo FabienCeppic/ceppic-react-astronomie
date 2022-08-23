04b38100f35fb07941c17f623dc4c924f8df77b5
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-08-23 12:18:43 +0200

    Accueil

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      Le but de ce blog est de créer des articles et de les lister, le contenu
+      des articles à pour thème l'astronomie.
+    </div>
+  );
+}
