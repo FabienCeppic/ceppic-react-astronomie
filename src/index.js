59387ad72edd0d1f35b3393a3d8a217b723d73e4
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-08-25 10:58:35 +0200

    Nettoyage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home";
 import NotFound from "./components/NotFound";
